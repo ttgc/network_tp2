ba055e1a35796b2b21c5c1de9462238a40d2af40
ttgc <ttgc@users.noreply.github.com>
2019-10-21 12:28:30 -0400

    Update enemy read method

--- main/src/enemy.cpp
+++ main/src/enemy.cpp
@@ -22,4 +22,19 @@ void Enemy::write(OutputStream& stream)
 
 void Enemy::read(InputStream& stream)
 {
+	compression::UncompressorCollection uncompressors;
+	m_type = stream.ReadStr();
+	coordinates::compressed::Position compressedPosition;
+	coordinates::compressed::Quaternion compressedRotation;
+
+	compressedPosition.x = stream.Read<uint32_t>();
+	compressedPosition.y = stream.Read<uint32_t>();
+	compressedPosition.z = stream.Read<uint32_t>();
+	m_position = uncompressors.posUncompressor(compressedPosition);
+
+	compressedRotation.missing = stream.Read<coordinates::compressed::Quaternion::QuaternionParameter>();
+	compressedRotation.first = stream.Read<uint16_t>();
+	compressedRotation.second = stream.Read<uint16_t>();
+	compressedRotation.third = stream.Read<uint16_t>();
+	m_rotation = uncompressors.quaternionUncompressor(compressedRotation);
 }
