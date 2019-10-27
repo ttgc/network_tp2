f88073b07b79b058639942c3b58235f2dc0f3cf1
ttgc <ttgc@users.noreply.github.com>
2019-10-27 16:25:39 -0400

    Added getter and setters for enemy and player

--- main/include/enemy.hpp
+++ main/include/enemy.hpp
@@ -11,6 +11,14 @@ public:
 	void write(OutputStream& stream) override;
 	void read(InputStream& stream) override;
 
+	coordinates::Position getPosition() const { return m_position; }
+	std::string getType() const { return m_type; }
+	coordinates::Quaternion getRotation() const { return m_rotation; }
+
+	void setPosition(const coordinates::Position& pos) { m_position = pos; }
+	void setName(const std::string& type) { m_type = type; }
+	void setRotation(const coordinates::Quaternion& rot) { m_rotation = rot; }
+
 private:
 	coordinates::Position m_position = { 0,0,0 };
 	std::string m_type = "untyped";
--- main/src/client.cpp
+++ main/src/client.cpp
@@ -1,4 +1,6 @@
 #include "client.hpp"
+#include "player.hpp"
+#include "enemy.hpp"
 
 namespace client
 {
@@ -28,6 +30,26 @@ namespace client
                 std::for_each(vectObjects.begin(), vectObjects.end(), [this](GameObject Obj) 
                     {
                         std::cout << "Replication data : " << Obj.ClassID() << " received." << std::endl;
+						if (Obj.ClassID() == 1)
+						{
+							const auto player = reinterpret_cast<Player*>(&Obj);
+							const auto pos = player->getPosition();
+							const auto rot = player->getRotation();
+							std::cout << "Player " << player->getName() << ": (" << 
+								pos.x << ", " << pos.y << ", " << pos.z << ") - (" <<
+								rot.x << ", " << rot.y << ", " << rot.z << ", " << rot.w << ")" << 
+								std::endl;
+						}
+						else if (Obj.ClassID() == 2)
+						{
+							const auto enemy = reinterpret_cast<Enemy*>(&Obj);
+							const auto pos = enemy->getPosition();
+							const auto rot = enemy->getRotation();
+							std::cout << "Enemy " << enemy->getType() << ": (" <<
+								pos.x << ", " << pos.y << ", " << pos.z << ") - (" <<
+								rot.x << ", " << rot.y << ", " << rot.z << ", " << rot.w << ")" <<
+								std::endl;
+						}
                     }
                 );
             });
